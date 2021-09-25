c2313abd88e58411b2e4cc86a36d335569d8cd33
Haydrill <saldua.h@gmail.com>
2021-09-25 13:54:47 -0700

    Player model and movement scripts
    Added model and movement scripts into the repo

---
+++ Simple Project/Assets/PlayerMovementAssets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float rotationSpeed = 1f;
+    public Transform Target, Player;
+    float mouseX, mouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Called after update
+    void LateUpdate()
+    {
+        CameraControl();    
+    }
+
+    void CameraControl()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * rotationSpeed;
+        // clamp Y rotation
+        mouseY = Mathf.Clamp(mouseY, -35f, 60f);
+
+        transform.LookAt(Target);
+
+        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
+        Player.rotation = Quaternion.Euler(0f, mouseX, 0f);
+    }
+
+}
