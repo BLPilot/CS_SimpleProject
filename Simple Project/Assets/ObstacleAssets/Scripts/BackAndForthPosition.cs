3afca03c9c231f4123660c1936d0cc99dbacaee4
Pavel Delev <49105996+PDelev@users.noreply.github.com>
2021-09-25 15:56:29 -0700

    Added obstacles and their scripts
    pls work

---
+++ Simple Project/Assets/ObstacleAssets/Scripts/BackAndForthPosition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class BackAndForthPosition : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    [SerializeField]
+    float speed = 1f;
+
+    // Vector coordinates of direction object should move from its current position
+    [SerializeField]
+    float xDistance = 0f;
+    [SerializeField]
+    float yDistance = 0f;
+    [SerializeField]
+    float zDistance = 0f;
+
+    Vector3 startPoint = Vector3.zero;
+    Vector3 endPoint = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacle = gameObject;
+        startPoint = obstacle.transform.position;
+        endPoint = startPoint + new Vector3(xDistance, yDistance, zDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * speed, 1);
+        obstacle.transform.position = Vector3.Lerp(startPoint, endPoint, time);
+    }
+}
---
+++ Simple Project/Assets/ObstacleAssets/Scripts/BackAndForthRotation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+public class BackAndForthRotation : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    [SerializeField]
+    float rotationSpeed = 1f;
+
+    // Vector coordinates of angles object should rotate from its current euler angles
+    [SerializeField]
+    [Range(-360,360)]
+    float xAngle = 0f;
+    [SerializeField]
+    [Range(-360, 360)]
+    float yAngle = 0f;
+    [SerializeField]
+    [Range(-360, 360)]
+    float zAngle = 0f;
+
+    Vector3 startAngles = Vector3.zero;
+    Vector3 endAngles = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacle = gameObject;
+        startAngles = obstacle.transform.eulerAngles;
+        endAngles = startAngles + new Vector3(xAngle, yAngle, zAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Mathf.PingPong(Time.time * rotationSpeed, 1);
+        obstacle.transform.eulerAngles = Vector3.Lerp(startAngles, endAngles, time);
+    }
+}
