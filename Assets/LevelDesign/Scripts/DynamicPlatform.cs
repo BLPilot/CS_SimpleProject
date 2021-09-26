010b91c048c4870b3ba196ee315a87c9ae9f1c77
Jonathan Peters <jonathan.peters99@gmail.com>
2021-09-25 17:36:47 -0700

    Level Design / Models

---
+++ Assets/LevelDesign/Scripts/DynamicPlatform.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicPlatform : MonoBehaviour
+{
+    public List<Transform> targets;
+    public float speed;
+    private int target = 0;
+    private float diff;
+    private Vector3 direction;
+
+    void Start()
+    {
+        target = -1;
+        NextTarget();
+    }
+
+    void NextTarget()
+    {
+        target = (target < targets.Count - 1) ? target + 1 : 0;
+        Debug.Log("target: " + target);
+        diff = (transform.localPosition - targets[target].localPosition).magnitude;
+        direction = (targets[target].localPosition - transform.localPosition).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(targets.Count);
+        transform.localPosition += direction * Time.deltaTime * speed;
+        if((transform.localPosition - targets[target].localPosition).magnitude < 0.1f)
+        {
+            NextTarget();
+        }
+    }
+}
