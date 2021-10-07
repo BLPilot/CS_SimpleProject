4d13cc2db13f16142c9dec485efc21ed943fdc4b
FMorales-afk <francisco_morales15@me.com>
2021-10-06 18:06:27 -0700

    NewCommit
    Tutorial menu updated

--- Assets/ScoreAssets/Distance.cs
+++ Assets/ScoreAssets/Distance.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 public class Distance : MonoBehaviour
 {
 	public Transform player;
-	public Text distanceText;
+	
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(player.position.z);
-		distanceText.text = player.position.z.ToString("0");
+		player.position.z.ToString("0");
 		
     }
 	
